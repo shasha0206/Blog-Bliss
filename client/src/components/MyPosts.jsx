d2f22981bdffff7c8fe92826436917f9b04660e7
shashank <165700757+shasha0206@users.noreply.github.com>
2024-12-02 22:13:48 +0530

    Update MyPosts.jsx

--- client/src/components/MyPosts.jsx
+++ client/src/components/MyPosts.jsx
@@ -29,7 +29,7 @@ const MyPosts = () => {
       }
 
       // Make the API call to fetch posts
-      const response = await axios.post('http://localhost:3000/myposts', 
+      const response = await axios.post('https://blog-bliss-backend.onrender.com/myposts', 
         {},
         {
           headers: {
